ce6596c15adcd5bd5b2dba7db109f79e68d19caa
mak-turing <makmiller@gmail.com>
2019-08-15 14:22:15 -0400

    update var

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ jinja_env = jinja2.Environment(autoescape=True,
 
 date_today = datetime.datetime.now() # .strftime('%Y, %m, %d')
 week_today = date_today.isocalendar()[1]
-week_first = datetime.date(2019, 1, 28).isocalendar()[1]
+week_first = datetime.date(2019, 8, 26).isocalendar()[1]
 week_jump = (week_today - week_first) + 1
 # week_jump = 2
 id_jump = 'Week' + str(week_jump)
