00641e79e665a0efc78473d8be077bb5618e3e06
mak-turing <makmiller@gmail.com>
2022-02-03 12:17:06 -0500

    Fix another typo

--- scripts/html-parser.py
+++ scripts/html-parser.py
@@ -44,7 +44,7 @@ def processHTML(path_to_tex):
 path_res = os.path.join(path_web, 'latex/research/research.tex')
 path_pubs = os.path.join(path_web, 'latex/my-pubs/my-pubs.tex')
 
-path_sched1 = os.path.join(path_web, 'latex/schedules/schedule_s22_phil255/schedule_s21_phil255.tex')
+path_sched1 = os.path.join(path_web, 'latex/schedules/schedule_s22_phil255/schedule_s22_phil255.tex')
 # path_sched2 = os.path.join(path_web, 'latex/schedules/schedule_f18_phil342/schedule_f18_phil342.tex')
 # path_sched3 = os.path.join(path_web, 'latex/schedules/schedule_s20_phil212/schedule_s20_phil212.tex')
 # path_sched4 = os.path.join(path_web, 'latex/schedules/schedule_s19_phil460/schedule_s19_phil460.tex')
