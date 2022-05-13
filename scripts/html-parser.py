1b03355daf2f74067676d56c4b7bfb8aecf4ad1f
mak-turing <makmiller@gmail.com>
2022-05-13 15:27:24 -0400

    Update my pub list

--- scripts/html-parser.py
+++ scripts/html-parser.py
@@ -53,9 +53,9 @@ path_sched6 = os.path.join(path_web, 'latex/schedules/schedule_s22_phil342/sched
 
 
 # processHTML(path_res)
-# processHTML(path_pubs)
-processHTML(path_sched1)
+processHTML(path_pubs)
+# processHTML(path_sched1)
 # processHTML(path_sched2)
 # processHTML(path_sched3)
 # processHTML(path_sched4)
-processHTML(path_sched6)
+# processHTML(path_sched6)
