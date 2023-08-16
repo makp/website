470c36bf567fdc68ecdf8c6e7a943d3e16024474
mak-turing <makmiller@gmail.com>
2023-08-16 14:02:22 -0400

    Create Python module to store common vars for courses

---
+++ courses_info.py
@@ -0,0 +1,10 @@
+semesters = {
+    'Fall_2023': {
+        'course_name': 'Introduction to Ethics',
+        'course_number': 'PHIL103',
+        'schedule_tex': '',
+        'schedule_html': '',
+        'syllabus_tex': '',
+        'syllabus_url': '',
+    }
+}
