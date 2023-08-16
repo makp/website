dfad3dc75fb2632de34a867bc36fd9843a46ca93
mak-turing <makmiller@gmail.com>
2023-08-16 15:08:03 -0400

    Don't produce an error when description is empty

--- courses_info.py
+++ courses_info.py
@@ -9,7 +9,7 @@ semesters = {
             'schedule_html': 'includes/schedule_f23_phil103.html',
             'syllabus_tex': '',
             'syllabus_url': '',
-            'description': 'includes/course-description-phil255.html'
+            'description': ''
         },
         'PHIL212': {
             'course_name': 'Honors Seminar',
@@ -17,5 +17,5 @@ semesters = {
             'schedule_html': 'includes/schedule_f23_phil103.html',
             'syllabus_tex': '',
             'syllabus_url': '',
-            'description': 'includes/course-description-phil255.html'
+            'description': ''
         }}}
