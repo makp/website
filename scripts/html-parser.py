c9c62bdf246caf7f6fdc8f559f05ebecb9fefea5
mak-turing <makmiller@gmail.com>
2023-08-17 11:47:56 -0400

    Update script

--- scripts/html-parser.py
+++ scripts/html-parser.py
@@ -44,19 +44,14 @@ def processHTML(path_to_tex):
 path_res = os.path.join(path_web, 'latex/research/research.tex')
 path_pubs = os.path.join(path_web, 'latex/my-pubs/my-pubs.tex')
 
-path_sched1 = os.path.join(path_web, 'latex/schedules/schedule_s23_phil255/schedule_s23_phil255.tex')
+path_sched1 = os.path.join(path_web, 'latex/schedules/schedule_f23_phil103/schedule_f23_phil103.tex')
 # path_sched2 = os.path.join(path_web, 'latex/schedules/schedule_f22_phil342/schedule_f22_phil342.tex')
 # path_sched3 = os.path.join(path_web, 'latex/schedules/schedule_s20_phil212/schedule_s20_phil212.tex')
 # path_sched4 = os.path.join(path_web, 'latex/schedules/schedule_s19_phil460/schedule_s19_phil460.tex')
 # path_sched5 = os.path.join(path_web, 'latex/schedules/schedule_f22_phil255/schedule_f22_phil255.tex')
-path_sched6 = os.path.join(path_web, 'latex/schedules/schedule_s23_phil342/schedule_s23_phil342.tex')
+path_sched6 = os.path.join(path_web, 'latex/schedules/schedule_f23_phil212/schedule_f23_phil212.tex')
 
 
 # processHTML(path_res)
 # processHTML(path_pubs)
-processHTML(path_sched1)
-# processHTML(path_sched2)
-# processHTML(path_sched3)
-# processHTML(path_sched4)
-# processHTML(path_sched5)
-# processHTML(path_sched6)
+processHTML(path_sched6)
