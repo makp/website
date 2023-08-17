39770fe5126f92fe6a6637d0656bb24e45420645
mak-leibniz <makmiller@gmail.com>
2023-08-16 23:27:52 -0400

    Refactor code

--- scripts/tex_processing.py
+++ scripts/tex_processing.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 
-def run_latex(tex_path):
+def run_command(tex_path, command, *args):
     original_dir = os.path.dirname(os.path.abspath(tex_path))
     current_working_dir = os.getcwd()
     base = os.path.basename(tex_path)
@@ -11,41 +11,22 @@ def run_latex(tex_path):
     os.chdir(original_dir)
 
     try:
-        subprocess.run(['latex', base_name], check=True)
+        subprocess.run([command, base_name, *args], check=True)
     except subprocess.CalledProcessError as e:
-        print("Error running LaTeX:", e)
+        print(f"Error running {command}:", e)
     finally:
         os.chdir(current_working_dir)
+
+
+def run_latex(tex_path):
+    run_command(tex_path, 'latex')
+
 
 
 def run_biber(tex_path):
-    original_dir = os.path.dirname(os.path.abspath(tex_path))
-    current_working_dir = os.getcwd()
-    base = os.path.basename(tex_path)
-    base_name, _ = os.path.splitext(base)
-
-    os.chdir(original_dir)
-
-    try:
-        subprocess.run(['biber', base_name], check=True)
-    except subprocess.CalledProcessError as e:
-        print("Error running Biber:", e)
-    finally:
-        os.chdir(current_working_dir)
+    run_command(tex_path, 'biber')
 
 
 def run_htlatex(tex_path):
-    original_dir = os.path.dirname(os.path.abspath(tex_path))
     config_path = os.path.abspath('./scripts/myconfig.cfg')
-    current_working_dir = os.getcwd()
-    base = os.path.basename(tex_path)
-    base_name, _ = os.path.splitext(base)
-
-    os.chdir(original_dir)
-
-    try:
-        subprocess.run(['htlatex', base_name, config_path, '-css', '-NoFonts'], check=True)
-    except subprocess.CalledProcessError as e:
-        print("Error running htlatex:", e)
-    finally:
-        os.chdir(current_working_dir)
+    run_command(tex_path, 'htlatex', config_path, '-css', '-NoFonts')
