de27007b8d0dcafa9bf2a00117203e91238f31e5
mak-leibniz <makmiller@gmail.com>
2023-08-16 23:41:42 -0400

    Don't switch working dir back and forth

--- scripts/tex_processing.py
+++ scripts/tex_processing.py
@@ -3,30 +3,32 @@ import subprocess
 
 
 def run_command(tex_path, command, *args):
-    original_dir = os.path.dirname(os.path.abspath(tex_path))
-    current_working_dir = os.getcwd()
+    """
+    Run a specified command with the given arguments.
+    """
+    tex_dir = os.path.dirname(os.path.abspath(tex_path))
+
     base = os.path.basename(tex_path)
     base_name, _ = os.path.splitext(base)
 
-    os.chdir(original_dir)
-
     try:
-        subprocess.run([command, base_name, *args], check=True)
+        subprocess.run([command, base_name, *args], check=True, cwd=tex_dir)
     except subprocess.CalledProcessError as e:
         print(f"Error running {command}:", e)
-    finally:
-        os.chdir(current_working_dir)
 
 
 def run_latex(tex_path):
     run_command(tex_path, 'latex')
 
 
-
 def run_biber(tex_path):
+    """
+    Run Biber on a .bcf file.
+    """
     run_command(tex_path, 'biber')
 
 
+# TODO: Consider using pandoc instead of htlatex.
 def run_htlatex(tex_path):
     config_path = os.path.abspath('./scripts/myconfig.cfg')
     run_command(tex_path, 'htlatex', config_path, '-css', '-NoFonts')
