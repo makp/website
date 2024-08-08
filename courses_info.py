4b6d3ad481690da37efed570ea8f2559b5b78340
mak-turing <makmiller@gmail.com>
2024-08-08 11:15:25 -0400

    Update list of courses for fall 24 semester

--- courses_info.py
+++ courses_info.py
@@ -1,39 +1,59 @@
-CURRENT_SEMESTER = 'Spring_2024'
+CURRENT_SEMESTER = "Fall_2024"
 
 
 semesters = {
-    'Fall_2023': {
-        'PHIL103': {
-            'course_name': 'Introduction to Ethics',
-            'schedule_tex': '',
-            'schedule_html': 'includes/schedule_f23_phil103.html',
-            'syllabus_tex': '',
-            'syllabus_url': '',
-            'description': 'includes/course-description-phil103.html'
+    "Fall_2024": {
+        "PHIL255": {
+            "course_name": "Environmental Ethics",
+            "schedule_tex": "",
+            "schedule_html": "includes/schedule_f24_phil255.html",
+            "syllabus_tex": "",
+            "syllabus_url": "",
+            "description": "includes/course-description-phil255.html",
         },
-        'PHIL212': {
-            'course_name': 'Honors Seminar',
-            'schedule_tex': '',
-            'schedule_html': 'includes/schedule_f23_phil212.html',
-            'syllabus_tex': '',
-            'syllabus_url': '',
-            'description': 'includes/course-description-phil212.html'
-        }},
-    'Spring_2024': {
-        'PHIL255': {
-            'course_name': 'Environmental Ethics',
-            'schedule_tex': '',
-            'schedule_html': 'includes/schedule_s24_phil255.html',
-            'syllabus_tex': '',
-            'syllabus_url': '',
-            'description': 'includes/course-description-phil255.html'
+        "PHIL342": {
+            "course_name": "What Makes Us Moral",
+            "schedule_tex": "",
+            "schedule_html": "",
+            "syllabus_tex": "",
+            "syllabus_url": "",
+            "description": "includes/course-description-phil342.html",
         },
-        'PHIL212': {
-            'course_name': 'Honors Seminar',
-            'schedule_tex': '',
-            'schedule_html': 'includes/schedule_s24_phil212.html',
-            'syllabus_tex': '',
-            'syllabus_url': '',
-            'description': 'includes/course-description-phil212.html'
-        }}
+    },
+    "Spring_2024": {
+        "PHIL255": {
+            "course_name": "Environmental Ethics",
+            "schedule_tex": "",
+            "schedule_html": "includes/schedule_s24_phil255.html",
+            "syllabus_tex": "",
+            "syllabus_url": "",
+            "description": "includes/course-description-phil255.html",
+        },
+        "PHIL212": {
+            "course_name": "Honors Seminar",
+            "schedule_tex": "",
+            "schedule_html": "includes/schedule_s24_phil212.html",
+            "syllabus_tex": "",
+            "syllabus_url": "",
+            "description": "includes/course-description-phil212.html",
+        },
+    },
+    "Fall_2023": {
+        "PHIL103": {
+            "course_name": "Introduction to Ethics",
+            "schedule_tex": "",
+            "schedule_html": "includes/schedule_f23_phil103.html",
+            "syllabus_tex": "",
+            "syllabus_url": "",
+            "description": "includes/course-description-phil103.html",
+        },
+        "PHIL212": {
+            "course_name": "Honors Seminar",
+            "schedule_tex": "",
+            "schedule_html": "includes/schedule_f23_phil212.html",
+            "syllabus_tex": "",
+            "syllabus_url": "",
+            "description": "includes/course-description-phil212.html",
+        },
+    },
 }
